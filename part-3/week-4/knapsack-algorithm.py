c52e9c724f5b8c37d152c465519a4a6417c21058
n.galanov <nikolay.galanov@teamdev.com>
2018-09-23 16:34:14 +0300

    Remove a redundant week

--- part-3/week-4/knapsack-algorithm.py
+++ part-3/week-4/knapsack-algorithm.py
@@ -1,4 +1,5 @@
 from utils.dataprovider import get
+import time
 
 
 def parse(data):
@@ -11,26 +12,47 @@ def parse(data):
     ]
 
 
-def compute_optimal_knapsack_value(capcity, items):
+def compute_optimal_knapsack_value(capacity, items):
+    min_weight = min(items, key=lambda item: item['weight'])['weight']
     amount = len(items)
-    matrix = [[0 for i in range(0, capacity)] for j in range(0, amount)]
+    matrix = [[0 for i in range(0, capacity)] for j in range(0, 2)]
 
     for i in range(1, amount):
+        iter_time = time.time()
+
         weight = items[i]['weight']
         value = items[i]['value']
-        for j in range(0, capacity):
-            matrix[i][j] = max(matrix[i-1][j], matrix[i-1][j - weight] + value)
 
-    return matrix[amount - 1][capacity - 1]
+        for j in range(min_weight, capacity):
+            value1 = matrix[0][j]
+            value2 = matrix[0][j - weight] + value if j - weight > -1 else value
+            matrix[1][j] = max(value1, value2)
+
+        matrix[0] = matrix[1].copy()
+
+        unit = time.time() - iter_time
+        print("\n--- Iteration №%d took %f seconds ---" % (i, unit))
+        print("--- Estimated time left: %f minutes ---" % ((amount - i + 1) * unit / 60))
+
+    return matrix[1][capacity - 1]
 
 
-raw = get('https://d3c33hcgiwev3.cloudfront.net/_6dfda29c18c77fd14511ba8964c2e265_knapsack1.txt?Expires=1537747200'
-          '&Signature=CPII3xWa~ASjHvMmlwoDURYHgCbHLXymz-8LEPMnQclYGm3E0~t9gNquuR2BM1VOb-44skwzN9afwT0USDZ'
-          'Iu~X0KvfzB1-~VoMC4Kl8e2BWzZPl8PJ8v-2XUo1XPGk-yBjSWdnA6hvaLe5aoW~pi~H32cAwuSSgYRDcDBWcBjo_&Key-P'
-          'air-Id=APKAJLTNE6QMUY6HBC5A')
+start_time = time.time()
+
+raw = get('https://d3c33hcgiwev3.cloudfront.net/_6dfda29c18c77fd14511ba8964c2e265_knapsack_big.txt?Expires=1'
+          '537747200&Signature=X00go7sESB~efQUSw-rfMP6KQdnXaQqM3ctuRcXkD~eAwhDLm-3lW9T8ScgudaMbaMwCH9J5joy8B'
+          'ATE8XXO5-C5MSpXRvKrDq9dnw6P~VkXr~8ReiW~JwaBpdCGsKmzCaWQhTVIuXix226RrcV-Vq1K6F4NF1EFnf3JOMqe5lY_'
+          '&Key-Pair-Id=APKAJLTNE6QMUY6HBC5A')
+
+# raw = get('https://d3c33hcgiwev3.cloudfront.net/_6dfda29c18c77fd14511ba8964c2e265_knapsack1.txt?Expires=15377472'
+#           '00&Signature=CPII3xWa~ASjHvMmlwoDURYHgCbHLXymz-8LEPMnQclYGm3E0~t9gNquuR2BM1VOb-44skwzN9afwT0USDZIu~X0K'
+#           'vfzB1-~VoMC4Kl8e2BWzZPl8PJ8v-2XUo1XPGk-yBjSWdnA6hvaLe5aoW~pi~H32cAwuSSgYRDcDBWcBjo_&Key-Pair-Id=APKAJL'
+#           'TNE6QMUY6HBC5A')
 
 capacity, items = parse(raw)
 
-print(compute_optimal_knapsack_value(capacity, items))
+print("\nResult: %s" % compute_optimal_knapsack_value(capacity, items))
 
-# 4471248
+print("\n--- Took %f seconds ---" % (time.time() - start_time))
+
+# 2571817
